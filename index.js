a7fc1a05f928dbc995fd2ae6b5e80f3ba3e78e2d
Wahid Ahmed Chowdhury <wahidchowdhury9000@gmail.com>
2023-05-31 20:22:55 +0600

    api created to insert appointments in databse

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ app.get("/", (req, res) => {
   res.send("DocHere server is running");
 });
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wy87kp4.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wy87kp4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,19 +28,25 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const doctorsCollection = client.db('docHereDB').collection('doctorsCollection');
+    const doctorsCollection = client.db("docHereDB").collection("doctorsCollection");
+    const bookingCollection = client.db("docHereDB").collection("bookingCollection");
 
+    app.get("/doctors", async (req, res) => {
+      const result = await doctorsCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/doctors', async(req, res) => {
-        const result = await doctorsCollection.find().toArray();
-        res.send(result)
-    })
+    app.get("/doctors/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await doctorsCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.get('/doctors/:id', async ( req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)}
-        const result = await doctorsCollection.findOne(query);
-        res.send(result);
+    app.post('/bookedAppointments', async(req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
