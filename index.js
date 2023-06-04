6b626cea03a5ea88cf1cce78d4db133ec27758ec
Wahid Ahmed Chowdhury <wahidchowdhury9000@gmail.com>
2023-06-05 01:58:54 +0600

    api created for specific user appointment and fetch all users data from db

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ async function run() {
 
 
     //users related API
+
+    app.get('/users', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
     app.post('/users', async(req, res) => {
       const user = req.body;
       const query = {email : user.email}
@@ -45,6 +52,7 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result)
     } )
+
 
 
     //doctors related API
