ef9345a209f407a5e8ad9acd750229a14d01d3f3
Wahid Ahmed Chowdhury <wahidchowdhury9000@gmail.com>
2023-06-05 23:32:42 +0600

    make admin api created

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const jwt = require('jsonwentoken')
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Unauthorized access" });
+  }
+
+  //bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401).send({ error: true, message: "Unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 app.get("/", (req, res) => {
   res.send("DocHere server is running");
@@ -29,51 +49,76 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const usersCollection = client.db("docHereDB").collection("usersCollection");
-    const doctorsCollection = client.db("docHereDB").collection("doctorsCollection");
-    const bookingCollection = client.db("docHereDB").collection("bookingCollection");
+    const usersCollection = client
+      .db("docHereDB")
+      .collection("usersCollection");
+    const doctorsCollection = client
+      .db("docHereDB")
+      .collection("doctorsCollection");
+    const bookingCollection = client
+      .db("docHereDB")
+      .collection("bookingCollection");
 
+    //jwt api   //token issue in backend done
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1hr",
+      });
+      res.send({ token });
+    });
 
     //users related API
-
-    app.get('/users', async(req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      console.log(existingUser);
+      if (existingUser) {
+        return res.send({ message: "User exists in database" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get('/users/admin/:email', verifyJWT,  async(req, res) => {
+      const email = req.params.email;
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        res.send({admin: false})
+      }
+
+      const query = {email : email}
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role  === 'admin'}
       res.send(result);
     })
 
 
-    app.post('/users', async(req, res) => {
-      const user = req.body;
-      const query = {email : user.email}
-      const existingUser = await usersCollection.findOne(query);
-      console.log(existingUser);
-      if(existingUser){
-        return res.send({message: 'User exists in database'})
-      }
-      const result = await usersCollection.insertOne(user);
-      res.send(result)
-    } )
-
-    app.patch('/users/admin/:id', async(req, res) => {
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          role: 'admin'
-        }
-      }
-      const result = await usersCollection.updateOne(filter, updateDoc)
-      res.send(result)
-    })
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
-    app.delete('/users/:id', async(req, res) => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await usersCollection.deleteOne(query);
-      res.send(result)
-    })
-
-
+      res.send(result);
+    });
 
     //doctors related API
     app.get("/doctors", async (req, res) => {
@@ -88,23 +133,29 @@ async function run() {
       res.send(result);
     });
 
-
     //booked appointment related API
-    app.post('/bookedAppointments', async(req, res) => {
+    app.post("/bookedAppointments", async (req, res) => {
       const booking = req.body;
       const result = await bookingCollection.insertOne(booking);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.get('/bookedAppointments', async(req, res) => {
+    app.get("/bookedAppointments",verifyJWT, async (req, res) => {
       const email = req.query.email;
-      if(!email){
+      if (!email) {
         res.send([]);
       }
-      const query = {patientEmail: email}
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "Forbidden access" });
+      }
+
+      const query = { patientEmail: email };
       const result = await bookingCollection.find(query).toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
