2157407c428bcffc9cb4927ec9029a49199f8e6b
Wahid Ahmed Chowdhury <wahidchowdhury9000@gmail.com>
2023-06-05 12:56:57 +0600

    api created to update and delete from all users in client side and installed jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require('jsonwentoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
@@ -52,6 +53,25 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result)
     } )
+
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
 
